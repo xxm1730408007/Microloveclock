bb6cf13abb68fcb56aaa1b6b6af94602bc5bce13
Meow <70001406+xxm1730408007@users.noreply.github.com>
2021-11-22 10:42:13 +0800

    Add files via upload

---
+++ pages/index.vue
@@ -0,0 +1,36 @@
+<template>
+	<view class="content">
+		<button type="default" @click="getDate">getDate</button>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: 'Hello'
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+			getDate(){
+				const db = uniCloud.database();
+				// 获取名为 `table1` 数据表的引用
+				db.collection("user").add({name: 'Ben'})
+				// const collection = db.collection('user');
+				// console.log(JSON.stringify(collection));
+			}
+		}
+	}
+</script>
+
+<style>
+	page{
+		background-color: skyblue;
+		width: 100%;
+		height: 2000rpx;
+		
+	}
+</style>
